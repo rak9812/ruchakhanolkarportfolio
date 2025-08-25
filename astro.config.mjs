9799b36610d18db02bd3014d8c4880590e8f3db6
Rucha Khanolkar <ruchakhanolkar@Ruchas-MacBook-Air.local>
2025-08-25 19:20:31 -0400

    Configure for custom domain ruchakhanolkar.com and fix all asset paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://rak9812.github.io',
-  base: '/ruchakhanolkarportfolio',
-  build: {
-    assets: '_astro',
-  },
+  site: 'https://ruchakhanolkar.com',
   integrations: [tailwind()],
 });
 
