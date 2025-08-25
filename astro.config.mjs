f50e21106523cce206df6844760ae3f0ac62a019
Rucha Khanolkar <ruchakhanolkar@Ruchas-MacBook-Air.local>
2025-08-25 17:29:51 -0400

    Configure GitHub Pages deployment with Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://rak9812.github.io',
+  base: '/ruchakhanolkarportfolio',
   integrations: [tailwind()],
 });
 
